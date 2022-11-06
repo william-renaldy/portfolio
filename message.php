bb410db30e273acf4520d719a3c8e7da68ac7a08
William Renaldy <93593129+william-renaldy@users.noreply.github.com>
2022-11-06 08:11:42 +0530

    Update message.php

--- message.php
+++ message.php
@@ -1,15 +1,17 @@
-//Contact Form in PHP
 <?php
+echo $_SERVER['REQUEST_METHOD'];
+
+echo "HII";
   $name = htmlspecialchars($_POST['name']);
   $email = htmlspecialchars($_POST['email']);
   $subject = htmlspecialchars($_POST['subject']);
-  $text = htmlspecialchars($_POST['text']);
+  $message = htmlspecialchars($_POST['text']);
 
-  if(!empty($email) && !empty($message)){
+  if((!empty($email)) && (!empty($message))){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
       $receiver = "williamrenaldy.a@gmail.com"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nSubject: $subject\n\nMessage:\n$text\n\nRegards,\n$name";
+      $body = "Name: $name\nEmail: $email\nSubject: $subject\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
          echo "Your message has been sent";
